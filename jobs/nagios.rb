adbc176e0f136059e9e282927be4b4495853fa89
Zach Oglesby <zach.oglesby@hungrymachine.com>
2016-02-03 09:00:33 -0500

    Added detailed panel for critical alerts

--- jobs/nagios.rb
+++ jobs/nagios.rb
@@ -12,15 +12,18 @@ SCHEDULER.every '30s' do
     unacked = nag.service_status(:host_status_types => [:all], :service_status_types => [:warning, :critical], :service_props => [:no_scheduled_downtime, :state_unacknowledged, :checks_enabled])
 
     critical_count = 0
+    critical_list = []
     warning_count = 0
     unacked.each do |alert|
       if alert["status"].eql? "CRITICAL"
         critical_count += 1
+        list_item = {:label => alert.host, :value => alert.service}
+        critical_list.push(list_item)
       elsif alert["status"].eql? "WARNING"
         warning_count += 1
       end
     end
-  
+
     # nagiosharder may not alert us to a problem querying nagios.
     # If no problems found check that we fetch service status and
     # expect to find more than 0 entries.
@@ -30,12 +33,13 @@ SCHEDULER.every '30s' do
         error = true
       end
     end
-  
+
     if error
       # Send an empty payload, which the widget treats as an error
       send_event('nagios-' + key.to_s, { })
     else
       send_event('nagios-' + key.to_s, { criticals: critical_count, warnings: warning_count })
+      send_event('nagios-' + key.to_s + '-crits', {items: critical_list})
     end
   end
 end
